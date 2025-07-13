c2d18660e33098f3bbc2f3cd29b3f23372800147
choiWebdev <choi.webdev@gmail.com>
2025-07-13 22:17:04 +0900

    2차 기능 구현 (숙지 여부, 단어 삭제)

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function UseFetch(url) {
+export default function useFetch(url) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
